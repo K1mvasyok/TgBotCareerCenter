f46e0ef8bf5e0005d22b3e94efedb38807c740a3
K1mvasyok <133892374+K1mvasyok@users.noreply.github.com>
2024-02-26 14:27:35 +0800

    start

---
+++ keyboadrs.py
@@ -0,0 +1,9 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+async def menu():
+    return ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="📖 "), KeyboardButton(text="📚 ")],
+            [KeyboardButton(text="🎓 "), KeyboardButton(text="📅 ")],], 
+        resize_keyboard=True, input_field_placeholder="Выберите пункт ниже")
