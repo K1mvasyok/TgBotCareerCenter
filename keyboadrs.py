6a67c22a35a498b4bc71e0fb1f532d3352ede189
K1mvasyok <133892374+K1mvasyok@users.noreply.github.com>
2024-03-18 14:33:10 +0800

    Update keyboadrs.py

--- keyboadrs.py
+++ keyboadrs.py
@@ -13,3 +13,7 @@ async def return_to_menu():
     return InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="🏡 Вернуться в меню", callback_data="return_to_menu")]])
     
 # Клавиатура со списком потоков 
+async def groups_by_potok():
+    airlines = await get_groups()
+    keyboard = [[InlineKeyboardButton(text=groups.name, callback_data=f'group:{group.id}')] for group in groups]
+    return InlineKeyboardMarkup(inline_keyboard=keyboard)
