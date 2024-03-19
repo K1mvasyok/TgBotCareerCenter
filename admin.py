d8e320aeb686c65edaf4324c8230e4b81e47257e
K1mvasyok <neancatvasia12@mail.ru>
2024-03-19 12:10:26 +0800

    da

--- admin.py
+++ admin.py
@@ -8,17 +8,19 @@ from config import ADMIN_TELEGRAM_ID
 router_a = Router()
 
 @router_a.message(Command("commands"))
-async def Start_commands_for_admin(message: Message) -> None:
+async def Cmd_start(message: Message):
     if message.from_user.id == ADMIN_TELEGRAM_ID:
-        await message.answer(f"Список всех доступных команд\n\n"
-                             f"/kurs - \n\n"
-                             f"/groups - \n\n"
-                             f"/ - \n\n"
-                             f"/ - \n\n")                       
+        await message.answer(f'Привет 👋🏼,\nЯ - чат-бот \n\n'
+                             f'Через меня можно написать ?сообщение?: \n\n'
+                             f'• Курсу\n\n'
+                             f'• Потоку\n\n'
+                             f'• Или конкретной группе\n\n')
+        await message.answer(f'🔮 Главное меню', reply_markup=await kb.menu())
     else:
         await message.answer("У вас нет прав на выполнение этой команды.")
 
-@router_a.message(F.text == '📖 Поток')
+# Работа для написания текста Курсу
+@router_a.message(F.text == '📖 Курс')
 async def Kurs(message: Message):
     if message.from_user.id == ADMIN_TELEGRAM_ID:
         await message.answer(f'Выберите курс которому вы бы хотели написать', reply_markup=await kb.kurs())  
@@ -29,3 +31,10 @@ async def Kurs(message: Message):
 async def Kyrs_bottons_act(query: CallbackQuery):
     kurs = int(query.data.split("_")[1])
     await query.message.answer(f'Kurs - {kurs}')
+    
+    
+# Работа для написания текста Потоку
+# План: Кнопка-Хэдлер
+
+
+# Работа для написания текста Группе
--- keyboards.py
+++ keyboards.py
@@ -12,13 +12,7 @@ async def menu():
 async def return_to_menu():
     return InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="🏡 Вернуться в меню", callback_data="return_to_menu")]])
     
-# Клавиатура со списком потоков 
-# async def groups_by_potok():
-#     airlines = await get_groups()
-#     keyboard = [[InlineKeyboardButton(text=groups.name, callback_data=f'group:{group.id}')] for group in groups]
-#     return InlineKeyboardMarkup(inline_keyboard=keyboard)
-
-
+# Клавиатура для выбора курса для курса
 async def kurs():
     buttons = [
         [InlineKeyboardButton(text="1 Курс", callback_data=f"kurs.number_1"),
