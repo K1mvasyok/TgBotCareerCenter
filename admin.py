cb5250881b1fe97e571782dc3f488646ac97840f
K1m <neancatvasia12@mail.ru>
2025-04-03 21:15:40 +0800

    Обновление к новой версии Aiogram, обновление бд, файлов

--- admin.py
+++ admin.py
@@ -5,7 +5,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 
 import keyboards as kb
-from requests import is_admin, add_admin, get_users_by_course, get_students_by_course_and_direction, get_users_by_group_id
+from requests import is_admin, add_admin, get_users_by_course, get_students_by_course_and_speciality, get_users_by_group_id
 from run import get_bot
 
 router_a = Router()
@@ -13,17 +13,17 @@ router_a = Router()
 class TextForKurs(StatesGroup):
     kurs = State()
     text = State()
-    
+
 class TextForPotok(StatesGroup):
     kurs = State()
     potok_id = State()
     text = State()
-    
+
 class TextForGroup(StatesGroup):
     kurs = State()
     potok_id = State()
-    group_id = State()    
-    text = State()       
+    group_id = State()
+    text = State()
 
 class AddAdmin(StatesGroup):
     telegram_id = State()
@@ -33,24 +33,24 @@ class AddAdmin(StatesGroup):
 async def Kurs(message: Message, state: FSMContext):
     user_id = message.from_user.id
     if await is_admin(user_id):
-        await message.answer(f'Выберите курс которому вы бы хотели написать', reply_markup=await kb.kurs())  
+        await message.answer(f'Выберите курс которому вы бы хотели написать', reply_markup=await kb.kurs())
     else:
         await message.answer("У вас нет прав на выполнение этой команды.")
 
 @router_a.callback_query(F.data.startswith("kurs.number_"))
 async def Kurs_bottons_act(query: CallbackQuery, state: FSMContext):
     kurs_id = int(query.data.split("_")[1])
-    await state.update_data(kurs=kurs_id) 
-    await query.message.answer(f'Введите сообщение для Курса')     
-    await state.set_state(TextForKurs.text)     
-    
+    await state.update_data(kurs=kurs_id)
+    await query.message.answer(f'Введите сообщение для Курса')
+    await state.set_state(TextForKurs.text)
+
 @router_a.message(TextForKurs.text)
 async def Kurs_text_act(message: Message, state: FSMContext):
     await state.update_data(text=message.text)
     data = await state.get_data()
-    kurs_id = data["kurs"]    
-    await message.answer(data["text"], reply_markup=await kb.ready_kurs(kurs_id))  
-    
+    kurs_id = data["kurs"]
+    await message.answer(data["text"], reply_markup=await kb.ready_kurs(kurs_id))
+
 @router_a.callback_query(F.data.startswith("kurs.ready_"))
 async def Kurs_ready_act(query: CallbackQuery, state: FSMContext):
     kurs_id = int(query.data.split("_")[1])
@@ -58,11 +58,11 @@ async def Kurs_ready_act(query: CallbackQuery, state: FSMContext):
     message_text = data["text"]
     users = await get_users_by_course(kurs_id)
     if users:
-            for user in users:
-                await send_message_to_user(user.telegram_id, message_text)
-            await query.message.answer("Сообщение успешно отправлено всем пользователям курса")
+        for user in users:
+            await send_message_to_user(user.telegram_id, message_text)
+        await query.message.answer("Сообщение успешно отправлено всем пользователям курса")
     else:
-            await query.message.answer("На выбранный курс не подписан ни один пользователь")
+        await query.message.answer("На выбранный курс не подписан ни один пользователь")
     await state.clear()
 
 # Работа для написания текста Потоку
@@ -70,29 +70,29 @@ async def Kurs_ready_act(query: CallbackQuery, state: FSMContext):
 async def Potok(message: Message, state: FSMContext):
     user_id = message.from_user.id
     if await is_admin(user_id):
-        await message.answer(f'Чтобы выбрать поток, выберите курс', reply_markup=await kb.potok_kurs())  
+        await message.answer(f'Чтобы выбрать поток, выберите курс', reply_markup=await kb.potok_kurs())
     else:
         await message.answer("У вас нет прав на выполнение этой команды.")
 
 @router_a.callback_query(F.data.startswith("potok.kurs.number_"))
 async def Potok_bottons_act(query: CallbackQuery, state: FSMContext):
     kurs_id = int(query.data.split("_")[1])
-    await state.update_data(kurs=kurs_id)        
-    await query.message.answer(f'Выберете поток', reply_markup=await kb.direction_for_curs(kurs_id))          
+    await state.update_data(kurs=kurs_id)
+    await query.message.answer(f'Выберете поток', reply_markup=await kb.speciality_for_curs(kurs_id))
 
-@router_a.callback_query(F.data.startswith("potok.direction_"))
+@router_a.callback_query(F.data.startswith("potok.speciality_"))
 async def Potok_text_act(query: CallbackQuery, state: FSMContext):
-    potok = int(query.data.split("_")[1])
-    await state.update_data(potok_id=potok)       
+    speciality_id = int(query.data.split("_")[1])
+    await state.update_data(potok_id=speciality_id)
     await query.message.answer(f'Введите сообщение для Потока')
-    await state.set_state(TextForPotok.text)    
+    await state.set_state(TextForPotok.text)
 
 @router_a.message(TextForPotok.text)
 async def Kurs_text_act(message: Message, state: FSMContext):
     await state.update_data(text=message.text)
     data = await state.get_data()
-    potok_id = data["potok_id"]  
-    await message.answer(data['text'], reply_markup=await kb.ready_direction(potok_id))   
+    potok_id = data["potok_id"]
+    await message.answer(data['text'], reply_markup=await kb.ready_speciality(potok_id))
 
 @router_a.callback_query(F.data.startswith("potok.ready_"))
 async def Potok_ready_act(query: CallbackQuery, state: FSMContext):
@@ -100,7 +100,7 @@ async def Potok_ready_act(query: CallbackQuery, state: FSMContext):
     data = await state.get_data()
     message_text = data["text"]
     course_id = data["kurs"]
-    users = await get_students_by_course_and_direction(course_id, potok_id)
+    users = await get_students_by_course_and_speciality(course_id, potok_id)
     if users:
         for user in users:
             await send_message_to_user(user.telegram_id, message_text)
@@ -110,42 +110,41 @@ async def Potok_ready_act(query: CallbackQuery, state: FSMContext):
     await state.clear()
 
 # Работа для написания текста Группе
-@router_a.message(F.text == '📚 Группа')
+@router_a.message(F.text == ' Группа')
 async def Group(message: Message, state: FSMContext):
     user_id = message.from_user.id
     if await is_admin(user_id):
-        await message.answer(f'Чтобы выбрать группу, выберите курс', reply_markup=await kb.kurs_group())  
+        await message.answer(f'Чтобы выбрать группу, выберите курс', reply_markup=await kb.kurs_group())
     else:
         await message.answer("У вас нет прав на выполнение этой команды.")
 
 @router_a.callback_query(F.data.startswith("group.number_"))
 async def Group_bottons_act(query: CallbackQuery, state: FSMContext):
     kurs_id = int(query.data.split("_")[1])
-    await state.update_data(kurs=kurs_id)        
-    await query.message.answer(f'Выберете поток', reply_markup=await kb.group_direction_for_curs(kurs_id)) 
+    await state.update_data(kurs=kurs_id)
+    await query.message.answer(f'Выберете поток', reply_markup=await kb.group_speciality_for_curs(kurs_id))
 
-@router_a.callback_query(F.data.startswith("group.direction_"))
+@router_a.callback_query(F.data.startswith("group.speciality_"))
 async def Group_2bot_act(query: CallbackQuery, state: FSMContext):
-    potok = query.data.split("_")[1]
-    await state.update_data(potok_id=potok) 
+    speciality_id = query.data.split("_")[1]
+    await state.update_data(potok_id=speciality_id)
     data = await state.get_data()
     kurs = data["kurs"]
-    # await query.message.answer(kurs)      
-    await query.message.answer(f'Выберете группу', reply_markup=await kb.generate_group_keyboard(kurs, potok))     
-    
+    await query.message.answer(f'Выберете группу', reply_markup=await kb.generate_group_keyboard(kurs, speciality_id))
+
 @router_a.callback_query(F.data.startswith("group.group_"))
 async def Group_text_act(query: CallbackQuery, state: FSMContext):
     group_id = int(query.data.split("_")[1])
-    await state.update_data(group_id=group_id)       
+    await state.update_data(group_id=group_id)
     await query.message.answer(f'Введите сообщение для Группы')
-    await state.set_state(TextForGroup.text)    
+    await state.set_state(TextForGroup.text)
 
 @router_a.message(TextForGroup.text)
 async def Group_text_do(message: Message, state: FSMContext):
     await state.update_data(text=message.text)
     data = await state.get_data()
-    group_id = data["group_id"]  
-    await message.answer(data['text'], reply_markup=await kb.ready_group(group_id))   
+    group_id = data["group_id"]
+    await message.answer(data['text'], reply_markup=await kb.ready_group(group_id))
 
 @router_a.callback_query(F.data.startswith("group.ready_"))
 async def Group_ready_act(query: CallbackQuery, state: FSMContext):
@@ -160,35 +159,35 @@ async def Group_ready_act(query: CallbackQuery, state: FSMContext):
     else:
         await query.message.answer("На выбранный курс не подписан ни один пользователь")
     await state.clear()
-        
+
 # Функция отправки сообщения пользователю
 async def send_message_to_user(user_id, message_text):
     bot = await get_bot()
     try:
         await bot.send_message(user_id, message_text)
-        return True  
+        return True
     except Exception as e:
         print(f"Не удалось отправить сообщение пользователю с ID {user_id}: {e}")
-        return False   
+        return False
 
-# Добавление нового администратора 
-@router_a.message(F.text == '📌 Добавление нового администратора')
+# Добавление нового администратора
+@router_a.message(F.text == ' Добавление нового администратора')
 async def Admin(message: Message, state: FSMContext):
     user_id = message.from_user.id
     if await is_admin(user_id):
-        await message.answer(f'Чтобы добавить администратора, нужен телеграмм id\n\nУзнать телеграмм id пользователя можно через бота @userinfobot\n\nДалее - отравьте 9-10 символов id аккаунта')  
-        await state.set_state(AddAdmin.telegram_id) 
+        await message.answer(f'Чтобы добавить администратора, нужен телеграмм id\n\nУзнать телеграмм id пользователя можно через бота @userinfobot\n\nДалее - отравьте 9-10 символов id аккаунта')
+        await state.set_state(AddAdmin.telegram_id)
     else:
         await message.answer("У вас нет прав на выполнение этой команды.")
-        
+
 @router_a.message(AddAdmin.telegram_id)
 async def Add_admin(message: Message, state: FSMContext):
     telegram_id = message.text
     if int(len(telegram_id)) == 9 or int(len(telegram_id)) == 10:
         result = await add_admin(telegram_id)
         if result:
-            await message.answer(f"Новый администратор успешно добавлен в базу данных", reply_markup= await kb.menu_a())
+            await message.answer(f"Новый администратор успешно добавлен в базу данных", reply_markup=await kb.menu_a())
         else:
-            await message.answer(f"Администратор с таким Telegram ID уже существует", reply_markup= await kb.menu_a())
+            await message.answer(f"Администратор с таким Telegram ID уже существует", reply_markup=await kb.menu_a())
     else:
-        await message.answer(f"Ошибка при добавлении администратора", reply_markup= await kb.menu_a())
+        await message.answer(f"Ошибка при добавлении администратора", reply_markup=await kb.menu_a())
--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from config import TOKEN
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.client.default import DefaultBotProperties
 
 import handlers
 import admin
@@ -10,20 +11,19 @@ import admin
 from models import async_main
 
 async def main():
-    bot = Bot(token=TOKEN, parse_mode='HTML')
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode='HTML'))
     dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(handlers.router_u)
     dp.include_router(admin.router_a)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
-    
+
 async def get_bot():
-    return Bot(token=TOKEN, parse_mode='HTML')
-    
+    return Bot(token=TOKEN, default=DefaultBotProperties(parse_mode='HTML'))
+
 if __name__ == '__main__':
     try:
         asyncio.run(async_main())
         asyncio.run(main())
     except (KeyboardInterrupt):
         print('Bot stopped!')
-        
