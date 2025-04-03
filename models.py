ad88c3c45d3aac249f30157d3004fd25c8a0f93b
K1m <neancatvasia12@mail.ru>
2025-04-03 21:43:41 +0800

    Обновление под новую бд

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
-from sqlalchemy import Column, Integer, String, BigInteger, ForeignKey
+from sqlalchemy import Column, Integer, String, BigInteger, ForeignKey, Table
 from sqlalchemy.orm import relationship, declarative_base
-from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
 from config import SQLALCHEMY_URL
 
@@ -9,49 +8,63 @@ engine = create_async_engine(SQLALCHEMY_URL, echo=True)
 async_session = AsyncSession(engine)
 Base = declarative_base()
 
+# Ассоциативная таблица для связи курсов и направлений (специальностей)
+course_speciality_association = Table(
+    'course_speciality_association',
+    Base.metadata,
+    Column('course_id', Integer, ForeignKey('courses.id'), primary_key=True),
+    Column('speciality_id', Integer, ForeignKey('specialities.id'), primary_key=True)
+)
+
+# Таблица для Курсов
 class Course(Base):
     __tablename__ = 'courses'
 
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True)
-    
-    groups = relationship("Group", back_populates="course")
 
-class Direction(Base):
-    __tablename__ = 'directions'
+    groups = relationship("Group", back_populates="course")
+    specialities = relationship("Speciality", secondary=course_speciality_association, back_populates="courses")
+
+# Таблица для Специальностей
+class Speciality(Base):
+    __tablename__ = 'specialities'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    
-    groups = relationship("Group", back_populates="direction")
+    name = Column(String, unique=True)
 
+    groups = relationship("Group", back_populates="speciality")
+    courses = relationship("Course", secondary=course_speciality_association, back_populates="specialities")
+
+# Таблица для Групп
 class Group(Base):
     __tablename__ = 'groups'
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    direction_id = Column(Integer, ForeignKey('directions.id'))
+    speciality_id = Column(Integer, ForeignKey('specialities.id'))
     course_id = Column(Integer, ForeignKey('courses.id'))
-    
-    direction = relationship("Direction", back_populates="groups")    
+
+    speciality = relationship("Speciality", back_populates="groups")
     course = relationship("Course", back_populates="groups")
     users = relationship("User", back_populates="group")
 
+# Таблица для Пользователей
 class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     telegram_id = Column(BigInteger)
     group_id = Column(Integer, ForeignKey('groups.id'))
-    
+
     group = relationship("Group", back_populates="users")
 
+# Таблица для Администраторов
 class Admin(Base):
     __tablename__ = 'admins'
 
     id = Column(Integer, primary_key=True)
     telegram_id = Column(BigInteger)
-
 
 async def async_main():
     async with engine.begin() as conn:
--- requests.py
+++ requests.py
@@ -2,34 +2,30 @@ from sqlalchemy import select, distinct, join
 from sqlalchemy.exc import IntegrityError
 from models import async_session
 
-from models import User, Direction, Group, Course, Admin
+from models import User, Speciality, Group, Course, Admin
 
 async def is_user_registered_db(telegram_id):
     async with async_session as session:
         query = select(User).where(User.telegram_id == telegram_id)
         result = await session.execute(query)
         return result.scalar() is not None
-    
-async def save_new_user(telegram_id, course_id, direction_id, group_id):
+
+async def save_new_user(telegram_id, course_id, speciality_id, group_id):
     async with async_session as session:
         try:
-            # Проверяем, существует ли пользователь с таким telegram_id
             existing_user = await session.execute(select(User).filter(User.telegram_id == telegram_id))
             existing_user = existing_user.scalar_one_or_none()
 
-            # Если пользователь существует, обновляем его данные
             if existing_user:
                 existing_user.group_id = group_id
                 await session.commit()
                 return True
 
-            # Проверяем существование курса, направления и группы
             course = await session.get(Course, course_id)
-            direction = await session.get(Direction, direction_id)
+            speciality = await session.get(Speciality, speciality_id)
             group = await session.get(Group, group_id)
 
-            # Если курс, направление и группа существуют, сохраняем нового пользователя
-            if course and direction and group:
+            if course and speciality and group:
                 new_user = User(telegram_id=telegram_id, group_id=group_id)
                 session.add(new_user)
                 await session.commit()
@@ -39,7 +35,7 @@ async def save_new_user(telegram_id, course_id, direction_id, group_id):
         except IntegrityError:
             await session.rollback()
             return False
-        
+
 async def get_user_data(telegram_id):
     async with async_session as session:
         query = select(User).where(User.telegram_id == telegram_id)
@@ -47,39 +43,39 @@ async def get_user_data(telegram_id):
         user = result.scalar()
         return user
 
-async def get_direction():
+async def get_specialities():
     async with async_session as session:
-        result = await session.execute(select(Direction))
+        result = await session.execute(select(Speciality))
         return result.scalars().all()
-    
-async def get_groups_by_course_and_direction(course_id, direction_id):
+
+async def get_groups_by_course_and_speciality(course_id, speciality_id):
     async with async_session as session:
         groups = await session.execute(
             select(Group).filter(
                 Group.course_id == course_id,
-                Group.direction_id == direction_id
+                Group.speciality_id == speciality_id
             )
         )
         group_records = groups.scalars().all()
         return group_records
-    
+
 async def get_group_info_by_id(group_id):
     async with async_session as session:
         result = await session.execute(
-            select(Group.name, Direction.name, Course.name)
-            .join(Direction)
+            select(Group.name, Speciality.name, Course.name)
+            .join(Speciality)
             .join(Course)
             .filter(Group.id == group_id)
         )
-        group_name, direction_name, course_name = result.first()
-        return group_name, direction_name, course_name
-    
+        group_name, speciality_name, course_name = result.first()
+        return group_name, speciality_name, course_name
+
 async def get_user_info_by_telegram_id(telegram_id):
     async with async_session as session:
         result = await session.execute(
-            select(Group.name, Direction.name, Course.name)
+            select(Group.name, Speciality.name, Course.name)
             .join(User.group)
-            .join(Direction)
+            .join(Speciality)
             .join(Course)
             .filter(User.telegram_id == telegram_id)
         )
@@ -88,7 +84,7 @@ async def get_user_info_by_telegram_id(telegram_id):
             return user_info
         else:
             return None
-        
+
 async def get_users_by_course(course_id):
     async with async_session as session:
         users = await session.execute(
@@ -96,80 +92,68 @@ async def get_users_by_course(course_id):
         )
         return users.scalars().all()
 
-async def get_direction_by_course_id(course_id):
+async def get_specialities_by_course_id(course_id):
     async with async_session as session:
-        # Выбираем уникальные направления для указанного курса
         stmt = (
-            select(distinct(Group.direction_id))
-            .select_from(join(Group, Course).join(Direction))
+            select(distinct(Group.speciality_id))
+            .select_from(join(Group, Course).join(Speciality))
             .where(Course.id == course_id)
         )
         result = await session.execute(stmt)
-        direction_ids = [row[0] for row in result]
-        
-        # Затем получаем сами объекты направлений по их идентификаторам
-        streams = await session.execute(select(Direction).where(Direction.id.in_(direction_ids)))
-        return streams.scalars().all()
-    
-async def get_students_by_course_and_direction(course_id, direction_id):
+        speciality_ids = [row[0] for row in result]
+
+        specialities = await session.execute(select(Speciality).where(Speciality.id.in_(speciality_ids)))
+        return specialities.scalars().all()
+
+async def get_students_by_course_and_speciality(course_id, speciality_id):
     async with async_session as session:
-        # Получаем идентификаторы групп для указанного курса и направления
         group_ids = await session.execute(
             select(Group.id)
             .join(Course)
-            .join(Direction)
-            .filter(Course.id == course_id, Direction.id == direction_id)
+            .join(Speciality)
+            .filter(Course.id == course_id, Speciality.id == speciality_id)
         )
         group_ids = [row[0] for row in group_ids.fetchall()]
 
-        # Получаем студентов из этих групп
         students = await session.execute(
             select(User)
             .join(Group)
             .filter(Group.id.in_(group_ids))
         )
         return students.scalars().all()
-    
-async def get_group_by_course_and_direction(course_id, direction_id):
+
+async def get_groups_by_course_and_speciality(course_id, speciality_id):
     async with async_session as session:
         groups = await session.execute(
             select(Group)
             .join(Course)
-            .join(Direction)
-            .filter(Course.id == course_id, Direction.id == direction_id)
+            .join(Speciality)
+            .filter(Course.id == course_id, Speciality.id == speciality_id)
         )
         return groups.scalars().all()
-    
+
 async def get_users_by_group_id(group_id):
     async with async_session as session:
         users = await session.execute(
             select(User)
             .filter(User.group_id == group_id)
         )
-        return users.scalars().all()    
-    
-# Проверка на администратора 
+        return users.scalars().all()
+
 async def is_admin(user_id):
     async with async_session as session:
-        # Получаем список всех идентификаторов администраторов из базы данных
         query = select(Admin.telegram_id)
         result = await session.execute(query)
         admin_ids = [row[0] for row in result.all()]
-        # Проверяем, является ли идентификатор пользователя администратором
         return user_id in admin_ids
 
-# Сохранение администратора
 async def add_admin(telegram_id):
     async with async_session as session:
         try:
-            # Создаем новую запись администратора
             new_admin = Admin(telegram_id=telegram_id)
             session.add(new_admin)
-            # Применяем изменения к базе данных
             await session.commit()
-            return True  # Успешно добавлен администратор
+            return True
         except IntegrityError:
-            # Обработка случая, когда администратор с таким telegram_id уже существует
             await session.rollback()
-            return False  # Администратор с таким telegram_id уже существу
-    
+            return False
